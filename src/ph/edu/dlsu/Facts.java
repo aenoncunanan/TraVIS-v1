59b0ee44e21b2f0cb9a89a42fd35a45b5e2061b0
aenoncunanan <aenon_cunanan2@yahoo.com>
2016-07-10 15:04:16 +0800

    Started adding data on facts section

--- src/ph/edu/dlsu/Facts.java
+++ src/ph/edu/dlsu/Facts.java
@@ -1,11 +1,19 @@
 package ph.edu.dlsu;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 
@@ -18,6 +26,11 @@ public class Facts {
     ScreenSize screen = new ScreenSize();
     double displayWidth = screen.getDisplayWidth();
     double displayHeight = screen.getDisplayHeight();
+
+    final TableView table = new TableView();
+
+    final ObservableList<Item> data =
+            FXCollections.observableArrayList();
 
     public Parent main(){
         Pane rootNode = new Pane();
@@ -35,12 +48,13 @@ public class Facts {
         grid.setPadding(new Insets(25, 25, 25, 25));
 
         Text message = new Text();
-        message.setText("Facts about traffic violations will appear here. Version of database should also appear here.");
+        message.setText("Source: http://www.mmda.gov.ph/index.php/20-faq/298-traffic-violations-and-penalties | As of: July 2016");
+        message.setFont(Font.font (8));
         message.setTextAlignment(TextAlignment.CENTER);
         grid.add(message, 0, 1);
 
-        grid.setTranslateX(420);
-        grid.setTranslateY(325);
+        grid.setTranslateX((displayHeight/2) + 90);
+        grid.setTranslateY((displayHeight/2) + 200);
 
         final CustomMenuItem home = new CustomMenuItem("home");
         final CustomMenuItem about = new CustomMenuItem("about");
@@ -68,9 +82,105 @@ public class Facts {
         menuBox.setTranslateX((displayWidth/2) - (200));
         menuBox.setTranslateY(630);
 
-        rootNode.getChildren().addAll(menuBox, grid);
+        table.setPrefSize(displayWidth/2, displayHeight/3);
+
+        TableColumn violation = new TableColumn("VIOLATIONS");
+        violation.setCellValueFactory(new PropertyValueFactory<>("violation"));
+        violation.setPrefWidth((table.getPrefWidth()/2) - 50);
+        violation.setSortable(false);
+
+        TableColumn penalty = new TableColumn("PENALTIES");
+        penalty.setCellValueFactory(new PropertyValueFactory<>("penalty"));
+        penalty.setPrefWidth((table.getPrefWidth()/2) + 50);
+
+        TableColumn first = new TableColumn("First Offense");
+        first.setCellValueFactory(new PropertyValueFactory<>("first"));
+        first.setPrefWidth(penalty.getPrefWidth()/3);
+        first.setSortable(false);
+
+        TableColumn second = new TableColumn("Second Offense");
+        second.setCellValueFactory(new PropertyValueFactory<>("second"));
+        second.setPrefWidth(penalty.getPrefWidth()/3);
+        second.setSortable(false);
+
+        TableColumn third = new TableColumn("Third Offense");
+        third.setCellValueFactory(new PropertyValueFactory<>("third"));
+        third.setPrefWidth(penalty.getPrefWidth()/3);
+        third.setSortable(false);
+
+        penalty.getColumns().addAll(first, second, third);
+
+        final ViolationList list = new ViolationList(data);
+
+        table.getColumns().addAll(violation, penalty);
+        table.setItems(data);
+
+        final VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(10, 0, 0, 10));
+        vbox.getChildren().addAll(table);
+        vbox.setTranslateX((displayWidth/2)-350);
+        vbox.setTranslateY(350);
+
+        rootNode.getChildren().addAll(menuBox, vbox, grid);
 
         return rootNode;
+    }
+
+    public static class Item{
+
+        private final SimpleStringProperty violation;
+        private final SimpleStringProperty first;
+        private final SimpleStringProperty second;
+        private final SimpleStringProperty third;
+
+        public Item(String violation, String first, String second, String third){
+            this.violation = new SimpleStringProperty(violation);
+            this.first = new SimpleStringProperty(first);
+            this.second = new SimpleStringProperty(second);
+            this.third = new SimpleStringProperty(third);
+        }
+
+        public String getViolation(){
+            return violation.get();
+        }
+
+        public void setViolation(String vio){
+            violation.set(vio);
+        }
+
+//        public String getPenalty(){
+//            return penalty.get();
+//        }
+//
+//        public void setPenalty(String vio){
+//            penalty.set(vio);
+//        }
+
+        public String getFirst(){
+            return first.get();
+        }
+
+        public void setFirst(String vio){
+            first.set(vio);
+        }
+
+        public String getSecond(){
+            return second.get();
+        }
+
+        public void setSecond(String vio){
+            second.set(vio);
+        }
+
+        public String getThird(){
+            return third.get();
+        }
+
+        public void setThird(String vio){
+            third.set(vio);
+        }
+
     }
 
 }
---
+++ src/ph/edu/dlsu/ViolationList.java
@@ -0,0 +1,223 @@
+package ph.edu.dlsu;
+
+import javafx.collections.ObservableList;
+
+/**
+ * Created by ${AenonCunanan} on 10/07/2016.
+ */
+public class ViolationList {
+    public ViolationList(ObservableList<Facts.Item> data) {
+        data.add(new Facts.Item(
+                "ALLOWING ANOTHER PERSON\nTO USE DRIVERS LICENSE",
+                "500.00\n1Mo Sus of DL",
+                "500.00\n1Mo Sus of DL",
+                "500.00\n1Mo Sus of DL"
+        ));
+        data.add(new Facts.Item(
+                "ALLOWING ANOTHER PERSON\nTO USE COMML / BUSS. NAME",
+                "375.00",
+                "375.00",
+                "375.00"
+        ));
+        data.add(new Facts.Item(
+                "ALLOWING IMPROPERLY\nLIC / UNLIC PERSON TO DRIVE",
+                "750.00",
+                "750.00",
+                "750.00"
+        ));
+        data.add(new Facts.Item(
+                "ALLOWING PASSENGER ON TOP OF VEHICLE",
+                "150.00",
+                "150.00",
+                "150.00"
+        ));
+        data.add(new Facts.Item(
+                "ARROGANCE / DISCOURTESY (w/ seminar)",
+                "150.00",
+                "150.00",
+                "150.00"
+        ));
+        data.add(new Facts.Item(
+                "BAN ON RIGHT-HAND DRIVE MV",
+                "500.00",
+                "500.00",
+                "500.00"
+        ));
+        data.add(new Facts.Item(
+                "BREACH OF FRANCHISE CONDITIONS",
+                "375.00",
+                "375.00",
+                "375.00"
+        ));
+        data.add(new Facts.Item(
+                "BROKEN SEALING WIRE",
+                "1 Mo Sus of DL\n1,000.00",
+                "3 Mos Sus of DL\n1,000.00",
+                "6 Mos Sus of DL\n1,000.00"
+        ));
+        data.add(new Facts.Item(
+                "BROKEN TAXIMETER SEAL",
+                "3 Mos Sus of DL\n2,500.00",
+                "6 Mos Sus of DL\n3,000.00",
+                "Rev of DL\n5,000.00\nRev of Fran"
+        ));
+        data.add(new Facts.Item(
+                "BUS / PUJ LANE ORDINANCE",
+                "200.00",
+                "200.00",
+                "200.00"
+        ));
+        data.add(new Facts.Item(
+                "BUS / PUJ Lane Ordinance along\n" +
+                        "Commonwealth(Pvt. Veh.) Physical App. \nMMDA Reg. No. 11-001 \nSeries of 2011",
+                "1,200.00",
+                "1,200.00",
+                "1,200.00"
+        ));
+        data.add(new Facts.Item(
+                "CARRYING RED LIGHTS INFRONT OF MV",
+                "150.00",
+                "150.00",
+                "150.00"
+        ));
+        data.add(new Facts.Item(
+                "COLORED / TINTED / PAINTED\nWINDSHIELD / WIND GLASS",
+                "500.00",
+                "500.00",
+                "500.00"
+        ));
+        data.add(new Facts.Item(
+                "COLORUM OPERATION (CARGO VEHICLE)",
+                "1,000.00\n3 Mos Conf Plt",
+                "2,000.00\n6 Mos Imp Mv",
+                "3,000.00\nCncl of Fran"
+        ));
+        data.add(new Facts.Item(
+                "COLORUM OPERATION (CARGO VEHICLE)",
+                "500.00\n3 Mos Sus DL",
+                "750.00\n6 Mos Sus DL",
+                "1,000.00\nRev of DL"
+        ));
+        data.add(new Facts.Item(
+                "COLORUM OPERATION (PASSENGER)\n- MMDA Reg. No. 97 - 004",
+                "5,000.00\n3 Mos Conf of Plt",
+                "5,000.00\n6 Mos Imp of Mv",
+                "5,000.00\nCncl of Fran"
+        ));
+        data.add(new Facts.Item(
+                "COLORUM OPERATION (PASSENGER)\n- MMDA Reg. No. 97 - 004",
+                "1,000.00\n3 Mos Sus of DL",
+                "1,000.00\n6 Mos Sus of DL",
+                "1,000.00\nRev of DL"
+        ));
+        data.add(new Facts.Item(
+                "CR / OR NOT CARRIED",
+                "150.00",
+                "150.00",
+                "150.00"
+        ));
+        data.add(new Facts.Item(
+                "CUTTING AN OVERTAKEN VEHICLE",
+                "150.00",
+                "150.00",
+                "150.00"
+        ));
+        data.add(new Facts.Item(
+                "DEFECTIVE BRAKES",
+                "150.00",
+                "150.00",
+                "150.00"
+        ));
+        data.add(new Facts.Item(
+                "DEFECTIVE EQUIPMENT",
+                "150.00",
+                "150.00",
+                "150.00"
+        ));
+        data.add(new Facts.Item(
+                "DEFECTIVE / BROKEN WINDSHIELD",
+                "150.00",
+                "150.00",
+                "150.00"
+        ));
+        data.add(new Facts.Item(
+                "DELINQUENT / INVALID REGISTRATION",
+                "450.00",
+                "450.00",
+                "450.00"
+        ));
+        data.add(new Facts.Item(
+                "DELINQUENT / INVALID REGISTRATION",
+                "450.00\nImp of Mv",
+                "450.00\nImp of Mv",
+                "450.00\nImp of Mv"
+        ));
+        data.add(new Facts.Item(
+                "DETACHED / IMPROPER / TEMPORARY SIGNBOARD",
+                "500.00",
+                "500.00",
+                "500.00"
+        ));
+        data.add(new Facts.Item(
+                "DIM-COLORED LIGHTS (FOR HIRE)",
+                "500.00",
+                "500.00",
+                "500.00"
+        ));
+        data.add(new Facts.Item(
+                "DIRTY / UNCARED FOR PLATES",
+                "150.00",
+                "150.00",
+                "150.00"
+        ));
+        data.add(new Facts.Item(
+                "DIRTY / UNSANITARY EQUIPMENT",
+                "150.00",
+                "150.00",
+                "150.00"
+        ));
+        data.add(new Facts.Item(
+                "DIRTY / UNSIGHTLY / DILAPIDATED MOTOR VEHICLE",
+                "150.00\nConf of Plt",
+                "150.00\nConf of Plt",
+                "150.00\nConf of Plt"
+        ));
+        data.add(new Facts.Item(
+                "DISREGARDING TRAFFIC SIGNS",
+                "150.00",
+                "150.00",
+                "150.00"
+        ));
+        data.add(new Facts.Item(
+                "DOUBLE LICENSE",
+                "300.00",
+                "300.00",
+                "300.00"
+        ));
+        data.add(new Facts.Item(
+                "Drag Racing / Speed Contest\n– PARANAQUE MMDA MC No. 14-11",
+                "5,000.00",
+                "5,000.00",
+                "5,000.00"
+        ));
+        data.add(new Facts.Item(
+                "Drag Racing / Speed Contest\n– PASAY MMDA MC No. 14-11",
+                "2,000.00",
+                "2,000.00",
+                "2,000.00"
+        ));
+        data.add(new Facts.Item(
+                "Dress Code for Riders (Slippers)",
+                "500.00",
+                "700.00",
+                "1,000.00"
+        ));
+        data.add(new Facts.Item(
+                "DRIVING AGAINST TRAFFIC",
+                "2,000.00\n3 Mos Sus of DL",
+                "2,000.00\n6 Mos Sus of DL",
+                "2,000.00\nRev of DL"
+        ));
+
+    }
+}
